7bf46f16e7f779d3c1be0653f8a8d4ed2f2a3412
valeriajanzen <janzen_valeria@outlook.com>
2022-12-20 04:34:34 -0300

    Adicionando as primeiras classes, aderindo a ultima versão do JPA 301

---
+++ Downloads/api (1)/api/src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -0,0 +1,5 @@
+package med.voll.api.endereco;
+
+public record DadosEndereco(String Logradouro, String bairro, String cep, String cidade, String uf, String complemento, String numero) {
+
+}
---
+++ Downloads/api (1)/api/src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,14 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Endereco {
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+}
